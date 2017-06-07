fbf53e9252f8e71ccff63d5f2e8344130156a379
mariaramyaa <mariaramya96@gmail.com>
2017-06-07 14:20:01 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,18 @@
+public class Java program
+{
+public static void main(String arg[])
+{
+char ch;
+Scanner scan =New scanner(System.in);
+System.out.print("Enter an cheractor:");
+ch=Scan.next().charAt(0);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+System.out.print("this is a vowel");
+}
+else
+{
+System.out.print("this is not a vowel");
+}
+}
+}
