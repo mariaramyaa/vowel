fe3b898097c9e048f572c70123238079e96ee4e8
mariaramyaa <mariaramya96@gmail.com>
2017-07-23 12:51:07 +0530

    Add files via upload

---
+++ Equalnum.java
@@ -0,0 +1,31 @@
+package ramya;
+import java.util.*;
+public class Equalnum {
+		  private static final int MAX = 100000;
+		  static void printpairs(int arr[],int sum)
+		  {
+		   
+		    boolean[] binmap = new boolean[MAX];
+		    for (int i=0; i<arr.length; ++i)
+		    {
+		      int temp = sum-arr2[i];
+		      
+		   
+		      if (temp>=0 && binmap[temp])
+		      {
+		        System.out.println("Pair with given sum " +
+		        sum + " is (" + arr2[i] +
+		        ", "+temp+")");
+		      }
+		      binmap[arr2[i]] = true;
+		    }
+		  }
+
+		  public static void main (String[] args)
+		  {
+		    int A[] = {1, 4, 45, 6, 10, 8};
+		    int n = 16;
+		    printpairs(A,  n);
+		  }
+		}
+}
