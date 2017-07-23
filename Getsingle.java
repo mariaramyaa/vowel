43a69d69a95196afdc2ff93de66acf153d3973c4
mariaramyaa <mariaramya96@gmail.com>
2017-07-23 09:57:18 +0530

    Add files via upload

--- Getsingle.java
+++ Getsingle.java
@@ -1,5 +1,26 @@
 package Guvi;
 
 public class Getsingle {
-
-}
+	 public static void main(String args[]) {
+		 int result = 0;
+    int x,int sum;
+ 
+    // Iterate through every bit
+    for (int i = 0; i < INT_SIZE; i++)
+    {
+      // Find sum of set bits at ith position in all
+      // array elements
+      sum = 0;
+      x = (1 << i);
+      for (int j=0; j< n; j++ )
+      {
+          if (arr[j] & x)
+            sum++;
+      }
+ 
+      // The bits with sum not multiple of 3, are the
+      // bits of element with single occurrence.
+      if (sum % 3)
+        result |= x;
+ 
+    return result;
